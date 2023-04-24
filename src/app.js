0d57180cb08396b888f56dc4754aa33814e661a2
Brais Fernández <braisoncrece@gmail.com>
2023-04-24 02:41:57 +0200

    [add] graphql & graphql playground

--- src/app.js
+++ src/app.js
@@ -1,35 +1,37 @@
 
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 
-const routerApi = require('./routes');
-const { checkApiKey } = require('./middlewares/auth.handler');
+const routerApi = require('./routes')
+const useGrapQl = require('./graphql')
+const { checkApiKey } = require('./middlewares/auth.handler')
 
-const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler')
 
-const createApp = () => {
-  const app = express();
+const createApp = async () => {
+  const app = express()
 
-  app.use(express.json());
-  app.use(cors());
+  app.use(express.json())
+  app.use(cors())
 
-  require('./utils/auth');
+  require('./utils/auth')
 
   app.get('/', (req, res) => {
-    res.send('Hola mi server en express');
-  });
+    res.send('Hola mi server en express')
+  })
 
   app.get('/nueva-ruta', checkApiKey, (req, res) => {
-    res.send('Hola, soy una nueva ruta');
-  });
+    res.send('Hola, soy una nueva ruta')
+  })
 
-  routerApi(app);
+  routerApi(app)
+  await useGrapQl(app)
 
-  app.use(logErrors);
-  app.use(ormErrorHandler);
-  app.use(boomErrorHandler);
-  app.use(errorHandler);
-  return app;
+  app.use(logErrors)
+  app.use(ormErrorHandler)
+  app.use(boomErrorHandler)
+  app.use(errorHandler)
+  return app
 }
 
-module.exports = createApp;
+module.exports = createApp
